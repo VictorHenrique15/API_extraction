baf1f66adb297784f31b654a2caa5eff32d3a36c
Victor <victorfer2008@gmail.com>
2025-09-19 18:05:36 -0300

    Adição estrutura do banco de dados e teste de funcionalidade

--- sql/estrutura.sql
+++ sql/estrutura.sql
@@ -1,1 +1,15 @@
-	
+create TABLE nasa_apod (
+	id SERIAL primary key,
+	title TEXT,
+	date DATE,
+	explanation TEXT,
+	url TEXT,
+	hdurl TEXT,
+	media_type TEXT,
+	copyright TEXT
+);
+
+
+select * from nasa_apod;
+
+SELECT * FROM nasa_apod ORDER BY date DESC;
